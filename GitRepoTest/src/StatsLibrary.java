ed2d05dd126f995e54da65e5c96bce474f6448ca
hdo28 <hdo28@10.0.0.96>
2023-01-29 22:05:04 -0500

    StatLibrary

---
+++ GitRepoTest/src/StatsLibrary.java
@@ -0,0 +1,90 @@
+
+public class StatsLibrary
+{
+	public StatsLibrary()
+	{
+		
+	}
+	public double mean(double[] find)
+	{
+		double total = 0;
+		for(int i = 0;i<find.length;i++)
+		{
+			total += find[i];
+		}
+		return total/find.length;
+	}
+	
+	public double median(double[] find)
+	{
+		double med = 0;
+		if(find.length%2==0)
+		{
+			med = (find[find.length/2] + find[(find.length/2) - 1])/2;
+		}
+		else
+		{
+			med = find[find.length/2];
+		}
+		return med;
+	}
+	
+	public String mode(double[] find)
+	{
+		int index = 0;
+		int count = 0;
+		int max = 1;
+		int max2 = 0;
+		for(int i = 0;i<find.length;i++)
+		{
+			for(int y = 0;y<find.length;y++)
+			{
+				if(find[y] == find[i])
+				{
+					count++;
+				}
+			}
+			if(count>max)
+			{
+				index = i;
+				max = count;
+			}
+			else if(count == max && find[i] != find[index])
+			{
+				max2 = count;
+			}
+			count = 0;
+		}
+		if(max == max2)
+		{
+			return "No mode";
+		}
+		else
+		{
+			String temp = String.valueOf(find[index]);
+			return temp;
+		}
+
+	}
+	
+	public double standard(double[] find)
+	{
+		double mean = 0;
+		double calc = 0;
+		
+		for(int i =0;i<find.length;i++)
+		{
+			mean += find[i];
+		}
+		
+		mean = mean/find.length;
+		
+		for(int i =0;i<find.length;i++)
+		{
+			calc += (find[i] - mean)*(find[i] - mean);
+		}
+		calc = calc/find.length;
+		return Math.sqrt(calc);
+	}
+	
+}
