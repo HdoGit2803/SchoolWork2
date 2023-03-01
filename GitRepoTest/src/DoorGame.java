0b9c1278d0a3c03a37bcef3bd2b80a1b91656c14
hdo28 <hdo28@10.0.0.85>
2023-03-01 01:12:12 -0500

    Added comments

--- GitRepoTest/src/DoorGame.java
+++ GitRepoTest/src/DoorGame.java
@@ -1,5 +1,11 @@
+/*
+ * program that calculate the chances of winning a game where there are 3 doors
+ * 1 door is the winning door other are losing doors
+ * choose 1 then the host opens one of the losing doors 
+ * then the player has a choice of changing his answer or not
+ * calculate the probability of winning if the player changed his answer and if he had not
+ */
 import java.util.Random;
-
 public class DoorGame
 {
 	private double changeW;
@@ -11,6 +17,15 @@ public class DoorGame
 		stayW = 0;
 	}
 	
+	/*
+	 * method to simulate door game 10000 times tallying how many times won if a person stayed or changed his choice
+	 * represent the doors in the game by designating them a number from 0-2
+	 * @param choice = choice of the player
+	 * @param win = the winning door
+	 * @param lose1 and lose2 = the 2 losing doors
+	 * @param open = the door that the host open
+	 * @param temp = a temp holder to represent the plater old choice if he decides to change
+	 */
 	public void playGame()
 	{
 		int choice;
@@ -23,36 +38,52 @@ public class DoorGame
 		
 		for(int i = 0;i<=10000;i++)
 		{
+			//generate the winning door by assigning it a number 0-2
 			win = rand.nextInt(3);
+			
+			//generate a losing door number 0-2 while the losing door # the same as the winning door # generate losing door again
 			lose1 = rand.nextInt(3);
 			while(lose1 == win)
 			{
 				lose1 = rand.nextInt(3);
 			}
 			
+			//generate a # 0-2 for the second lost door while loop so that it not the same # as the other 2 doors
 			lose2 = lose1;
 			while(lose2 == lose1 || lose2 == win)
 			{
 				lose2 = rand.nextInt(3);
 			}
 			
+			//generate a number 0-2 and that is the choice of the door chosen
 			choice = rand.nextInt(3);
+			/*
+			 * if the current chosen door is the same as the winning door then tally 1 win for staying
+			 * since in this scenario the player will not change his choice there no need
+			 * to have the program open a door yet
+			 */
 			if(choice == win)
 			{
 				stayW++;
 			}
 			
+			
+			//part 2 of the same program now assume that the player will now choose to change his choice
 			temp = choice;
 			open = choice;
+			//open a door while making sure it not the winning door or the chosen door
 			while(open == choice || open == win)
 			{
 				open = rand.nextInt(3);
 			} 
+			
+			//changes the door chosen while loop to assure that the changed choice is not the same # as the open door or the current choice
 			while(choice == open || choice == temp)
 			{
 				choice = rand.nextInt(3);
 			}
 
+			//if the choice # after changing choice is the same as the winning number tally 1 win for change
 			if(choice == win)
 			{
 				changeW++;
@@ -61,10 +92,13 @@ public class DoorGame
 		}
 	}
 	
+	//return the probability of winning if the player had changed their choice
 	public double getChange()
 	{
 		return (changeW/10000)*100;
 	}
+	
+	//return chance of winning if you chosen to not change your choice
 	public double getStay()
 	{
 		return (stayW/10000)*100;
--- GitRepoTest/src/Person.java
+++ GitRepoTest/src/Person.java
@@ -1,11 +1,16 @@
-import java.util.Random;
 
+import java.util.Random;
 public class Person
 {
 	private double match;
 	private double run;
 	private int student;
 
+	/*
+	 * @param student = the # of students in the class
+	 * @param run = the # of trials being done
+	 * @param match = the number of matches 1 per trial starts
+	 */
 	public Person(int stu,int ru)
 	{
 		match = 0;
@@ -13,21 +18,36 @@ public class Person
 		student = stu;
 	}
 	
+	/*
+	 * method to tally if a class of x student will have 2 people with matching birthday
+	 * out of y trials where
+	 * @param bDay = an array that represent students in the class and their birthdays
+	 * @param
+	 */
 	public void birthday()
 	{
 		Random rand = new Random();
+		//for loop the goes for however many trails needed determined by @run
 		for(int i = 0;i<=run;i++)
 		{
+			//create an array that represent the number of student in the class
 			int[] bDay =  new int[student];
+			//randomly assign each element that represent a different student with a birthday
 			for(int n = 0;n<student;n++)
 			{
 				bDay[n] = rand.nextInt(365);
 			}
 			
+			//loop that look at each element in the array
 				for(int y = 0;y<student;y++)
 				{
+					//loop that looks at all the following elements in the array
 					for(int x = y+1;x<student;x++)
 					{
+						/*
+						 * if the current element y has the same birthday as one of the following element x
+						 * then match+1 then end the current run of this trial since we only need 1 match
+						 */
 						if(bDay[y] == bDay[x])
 						{
 							match++;	
@@ -39,6 +59,7 @@ public class Person
 			}
 	}
 	
+	//calculate the probability of 2 people in a class having the same birthday then return that #
 	public double prob()
 	{
 		return (match/run)*100;
